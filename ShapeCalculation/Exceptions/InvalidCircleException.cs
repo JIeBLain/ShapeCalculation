b78c33db739a255bfb6ed980c4297074d35f3314
Lev <lev-sidorov@yandex.ru>
2023-11-13 23:04:53 +0700

    Add InvalidCircleException

---
+++ ShapeCalculation/Exceptions/InvalidCircleException.cs
@@ -0,0 +1,9 @@
+﻿namespace ShapeCalculation.Exceptions;
+
+public sealed class InvalidCircleException : Exception
+{
+    public InvalidCircleException(Circle circle)
+        : base($"The radius of the circle must be a positive number. Radius {circle.Radius} is not valid!")
+    {
+    }
+}
